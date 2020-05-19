9cd53b3cbab772dafc210b65b47b053a9d95d771
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-19 03:51:48 -0500

    Setup

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///test.db'
+db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "<h1>Assalomu aleykum</h1>"
+    return render_template("index.html")
 
 
 if __name__=="__main__":
